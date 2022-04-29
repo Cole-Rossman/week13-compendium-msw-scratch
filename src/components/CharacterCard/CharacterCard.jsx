e4508d8f7322601ab47c43c922eec05464e0bdeb
Cole Rossman <colerossman99@gmail.com>
2022-04-28 17:08:52 -0700

    css added for main and charactercard

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -7,7 +7,7 @@ export default function CharacterCard({ character, image, quote }) {
     <div key={ character } className={cardstyle.card}>
       <img alt='name' width="150" height="120" src={image} />
       <h3>{character}</h3>
-      <p>{quote}</p>
+      <p>"{quote}"</p>
     </div>
   )
 }
